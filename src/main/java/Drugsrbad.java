a9d7e4348bfc14c33ea0c0e61d988fbdbc697247
danielngan <ngan.yiching@gmail.com>
2021-04-28 16:09:05 +0800

    I add my chart to it.

---
+++ src/main/java/Drugsrbad.java
@@ -0,0 +1,14 @@
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
+public class Drugsrbad {
+    public static void main(String[] args) {
+        double[] xData = new double[] { 0.0, 1.0, 2.0, 7.0, 12.0, 9.5, 1.7, 8.5, 10.8, 43.8 };
+        double[] yData = new double[] { 2.0, 1.0, 0.0, 10.8, 7.9, 48.7, 9.4, 12.6, 9.4, 11.8};
+
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+        new SwingWrapper(chart).displayChart();
+    }
+
+}
