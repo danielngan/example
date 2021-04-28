ba9fdde9821874873c12f3fa3b937b2005c24a90
Vincent Ngan (Hutchison Ports, IT) <ngan.vincent@hutchisonports.com>
2021-04-28 16:34:05 +0800

    Change chart name to Smart Chart

--- src/main/java/Drugsrbad.java
+++ src/main/java/Drugsrbad.java
@@ -7,7 +7,7 @@ public class Drugsrbad {
         double[] xData = new double[] { 0.0, 1.0, 2.0, 7.0, 12.0, 9.5, 1.7, 8.5, 10.8, 43.8 };
         double[] yData = new double[] { 2.0, 1.0, 0.0, 10.8, 7.9, 48.7, 9.4, 12.6, 9.4, 11.8};
 
-        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+        XYChart chart = QuickChart.getChart("Smart Chart", "X", "Y", "y(x)", xData, yData);
         new SwingWrapper(chart).displayChart();
     }
 
